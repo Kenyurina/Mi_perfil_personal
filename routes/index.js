138fc440147da2bde01ff5ffc8f9934316dfc94d
Kenyuri Camino <kenyuri9@gmail.com>
2020-04-28 17:57:32 -0400

    Modificacion de las rutas y simplificacion de archivos .pug

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var express = require('express'),
     router = express.Router()
 
 router
-    .get('/', (req, res) => res.render('web.pug'))
+    .get('/', (req, res) => res.render('ppal.pug'))
+    .get('/proyectos', (req, res) => res.render('full_img_proy.pug'))
 
 module.exports = router
